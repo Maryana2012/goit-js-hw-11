3465b9a0585281fc9bc8e1aad8828ef2907e7a1e
Maryana2012 <maryana.oryshchuk@gmail.com>
2023-03-31 16:41:17 +0300

    async/await

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import './css/styles.css';
 import Notiflix from 'notiflix';
 import SimpleLightbox from "simplelightbox";
-// import {axiosRequest} from './axiosRequest';
 import "simplelightbox/dist/simple-lightbox.min.css";
 import axios from 'axios';
 
@@ -13,7 +12,7 @@ const btnLoadEl = document.querySelector('.load-more');
  
 const BASE_URL = 'https://pixabay.com/api/';
 const KEY_API = '34725568-3bb6c7550daf8cb631b41e469';
-let countPage = 1;
+// let countPage = 1;
 const perPage = 40;
 
 inputEl.addEventListener('input', handleReadInput);
@@ -33,6 +32,20 @@ function cleanerPage() {
     divEl.innerHTML = '';
 }
 
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 250 });
+    
+function smoothScroll() {
+    const { height: cardHeight } = document
+    .querySelector(".gallery")
+    .firstElementChild.getBoundingClientRect();
+    
+    window.scrollBy({
+    top: cardHeight * 2,
+    behavior: "smooth",
+    });
+}
+
 function handleReadInput(e) {
     const inputValue = inputEl.value.trim();
     if (inputValue === '') {
@@ -42,109 +55,72 @@ function handleReadInput(e) {
     } else { return inputValue; }
 }
 
+ const axiosPromise = async (name, countPage) => {
+     const response = await axios.get(`${BASE_URL}?key=${KEY_API}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${perPage}&page=${countPage}`); 
+     return response;
+ }
 
 function handleMakeBtnSearchImages(e) {
     e.preventDefault();
     const name = handleReadInput();
     let countPage = 1;
-    axios.get(`${BASE_URL}?key=${KEY_API}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${perPage}&page=${countPage}`)
-    .then(response => { 
-        const { data: { totalHits }, data:{hits}  } = response;
+    axiosPromise(name, countPage)
+        .then(response => { console.log(response);
+            const { data: { totalHits }, data: { hits } } = response;
         if ((totalHits !== 0) && (countPage === 1)) {
             Notiflix.Notify.info(`Hooray! We found ${totalHits} images.`);
         }
         if (totalHits !== 0) {
-            makeRender(response);
-            
+            makeRender(response); 
         } else {
             Notiflix.Notify.warning("Sorry, there are no images matching your search query. Please try again.") }     
-            
-            if (hits.length === 40) {
-                btnLoadMoreVisible();
-            }
-            // else {
-            //     btnLoadMoreIsHidden();
-            //     Notiflix.Notify.info("We're sorry, but you've reached the end of search results.")    }      
-            })
-            .catch(err => console.log(err));
+        if (hits.length === 40) {
+            btnLoadMoreVisible();
         }
+        smoothScroll();
+    })
+    .catch(err => console.log(err));
+}
         
 countPage = 2;
 function handleMakeBtnLoadMore() {
     const name = handleReadInput();
-    
-    axios.get(`${BASE_URL}?key=${KEY_API}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${perPage}&page=${countPage}`)
+    axiosPromise(name, countPage)
     .then(response => { 
-        const { data: { totalHits }, data: { hits } } = response;
-        const name = handleReadInput();
-        console.log(countPage);
-         if ((hits.length)  === perPage) {
-             console.log(hits.length);
-             countPage += 1;
-             makeRender(response);   
-         } else {
+        const { data: { hits } } = response;
+        if ((hits.length)  === perPage) {
+            countPage += 1;
+            makeRender(response);   
+            smoothScroll();
+        } else {
             Notiflix.Notify.info("We're sorry, but you've reached the end of search results.")  
-             makeRender(response);  
-             btnLoadMoreIsHidden();
-         }
-        
-            // if (hits.length === 40) {
-            //     btnLoadMoreVisible();
-            // } else {
-            //     btnLoadMoreIsHidden();
-            //     Notiflix.Notify.info("We're sorry, but you've reached the end of search results.")    }      
-            })
-            .catch(err => console.log(err));
+            makeRender(response);  
+            btnLoadMoreIsHidden();
+        }
+      })
+    .catch(err => console.log(err));
  }      
 
 function makeRender(response) {
     const markupImg = (({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
         return `
         <div class="photo-card">
-        <a href="${largeImageURL}"><img src="${webformatURL}" alt="${tags}" loading="lazy" width=120 /></a>`
-
-        // <div class="info">
-        // <p class="info-item">
-        // <b>Likes:${likes}</b>
-        //    </p>
-
-        //    <p class="info-item">
-        //    <b>Views:${views}</b>
-        //    </p>
-
-        //    <p class="info-item">
-        //    <b>Comments:${comments}</b>
-        //    </p>
-        
-        //   <p class="info-item">
-        //   <b>Downloads:${downloads}</b>
-        //   </p>
-        //   </div>
-        // </div> 
+        <figure> <a href="${largeImageURL}"><img src="${webformatURL}" alt="${tags}" loading="lazy" width=120 /></a>
+        <figcaption>
+         <div class="info">
+         <p class="info-item"><b>Likes:${likes}</b></p>
+         <p class="info-item"><b>Views:${views}</b></p>
+         <p class="info-item"><b>Comments:${comments}</b></p>
+         <p class="info-item"><b>Downloads:${downloads}</b></p>
+         </div>
+        </figcaption>
+        </figure>
+        </div>`
     });
     const markup = response.data.hits.map(data => markupImg(data)).join('');
-    divEl.insertAdjacentHTML('beforeend', markup);          
-    
+    divEl.insertAdjacentHTML('beforeend', markup);  
+    lightbox.refresh();
 }
 
-// const totalImgGetLS = localStorage.getItem('total');
-// const totalImgCount = JSON.parse(totalImgGetLS);
 
-// function handleMakeBtnSearchImages(e) {
-//     e.preventDefault();
-//     const name = handleReadInput();
-//     axiosRequest(name, countPage);
-//     Notiflix.Notify.info(`Hooray! We found ${totalImgCount} images.`);
-// }
 
-// function handleMakeBtnLoadMore() {
-//     const name = handleReadInput();
-//     if ((totalImgCount % perPage) > 0) {
-//         countPage += 1;
-//         axiosRequest(name, countPage);   
-//     } 
-// }
-
-// new SimpleLightbox('.gallery a', {
-//     captionsData: 250,
-// })
